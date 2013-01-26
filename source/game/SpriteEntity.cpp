b92e49fe51ede03db829109bc6991187c5aee8c2
Marlus Costa <marluscadanus@gmail.com>
2013-01-26 16:17:10 -0200

    Added WASD as a input too.

--- source/game/SpriteEntity.cpp
+++ source/game/SpriteEntity.cpp
@@ -18,10 +18,10 @@ SpriteEntity::~SpriteEntity()
 
 void SpriteEntity::Load(Seed::IMetadataObject &metadata, Seed::SceneNode *sprites)
 {
-    strName = metadata.GetProperty("Class");
+	strName = metadata.GetProperty("Class");
 
 	pSprite = New(Sprite(*static_cast<Sprite *>(sprites->GetChildByName(pszSpriteName))));
-    pSprite->SetPosition(metadata.GetPosition());
+	pSprite->SetPosition(metadata.GetPosition());
 
 	gScene->Add(pSprite);
 
@@ -35,33 +35,33 @@ void SpriteEntity::OnInputKeyboardPress(const EventInputKeyboard *ev)
 {
 	Key k = ev->GetKey();
 
-	if (k == Seed::KeyUp)
+	if (k == Seed::KeyUp || k == Seed::KeyW)
 	{
 		// Sum the normalized vector up with the current vector
 		vPlayerVectorDirection += VECTOR_UP;
 	}
 
-	if (k == Seed::KeyLeft)
+	if (k == Seed::KeyLeft || k == Seed::KeyA)
 	{
 		// Sum the normalized vector left with the current vector
 		vPlayerVectorDirection += VECTOR_LEFT;
-
-		// Change the scale to turn the player sprite
-		if (pSprite->GetScaleX() < 0)
-			pSprite->SetScaleX(pSprite->GetScaleX() * -1);
-	}
-
-	if (k == Seed::KeyRight)
-	{
-		// Sum the normalized vector right with the current vector
-		vPlayerVectorDirection += VECTOR_RIGHT;
 
 		// Change the scale to turn the player sprite
 		if (pSprite->GetScaleX() > 0)
 			pSprite->SetScaleX(pSprite->GetScaleX() * -1);
 	}
 
-	if (k == Seed::KeyDown)
+	if (k == Seed::KeyRight || k == Seed::KeyD)
+	{
+		// Sum the normalized vector right with the current vector
+		vPlayerVectorDirection += VECTOR_RIGHT;
+
+		// Change the scale to turn the player sprite
+		if (pSprite->GetScaleX() < 0)
+			pSprite->SetScaleX(pSprite->GetScaleX() * -1);
+	}
+
+	if (k == Seed::KeyDown || k == Seed::KeyS)
 	{
 		// Sum the normalized vector down with the current vector
 		vPlayerVectorDirection += VECTOR_DOWN;
